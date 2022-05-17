7fe7ca1b1ab7fc5357aa53cb2d9e29619e07f9a3
Red Hat - Sanderson Sa <sanderson.redhat@fornecedores.klabin.com.br>
2022-05-17 09:52:38 -0300

    alterar configs

--- src/main/java/xyz/sandersonsa/amqspringbootstarter/AmqSpringbootStarterApplication.java
+++ src/main/java/xyz/sandersonsa/amqspringbootstarter/AmqSpringbootStarterApplication.java
@@ -1,15 +1,43 @@
 package xyz.sandersonsa.amqspringbootstarter;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 
-@SpringBootApplication
 @EnableJms
-public class AmqSpringbootStarterApplication {
+@SpringBootApplication
+public class AmqSpringbootStarterApplication implements CommandLineRunner {
+    @Autowired
+    private JmsTemplate jmsTemplate;
 
-	public static void main(String[] args) {
-		SpringApplication.run(AmqSpringbootStarterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AmqSpringbootStarterApplication.class, args);
+    }
 
+    @Override
+    public void run(String... strings) throws Exception {
+        sendMessage("Hello World!");
+    }
+
+    public void sendMessage(String text) {
+        System.out.println(String.format("Sending '%s'", text));
+        this.jmsTemplate.convertAndSend("example", text);
+    }
+
+    @JmsListener(destination = "example")
+    public void receiveMessage(String text) {
+        System.out.println(String.format("Received '%s'", text));
+    }
 }
+
+// public class AmqSpringbootStarterApplication {
+
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(AmqSpringbootStarterApplication.class, args);
+// 	}
+
+// }
